184a229575fd68a23abd3a161f427e7b8cb72c76
maylimaxz <mayara.pinheiro.lima@escola.pr.gov.br>
2026-07-08 08:51:42 -0300

    090yy

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 geraSenha();
 
 function geraSenha(){
+    let senha = '';
  for (let i = 0; i < tamanhoSenha;i++) {
      let numeroAleatorio = Math.random()*letrasMaiusculas.length;
      numeroAleatorio = Math.floor(numeroAleatorio);
