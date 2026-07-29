cafeffdc0bd98a5ff39238062c7c7891f6716bd6
maylimaxz <mayara.pinheiro.lima@escola.pr.gov.br>
2026-07-29 08:19:25 -0300

    uygyukfghv

--- main.js
+++ main.js
@@ -1,39 +1,55 @@
+// Número de caracteres
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
 
+// Botões
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){ 
-   amanhoSenha = tamanhoSenha - 1;
-    numeroSenha.textContent = tamanhoSenha
-}
-function aumentaTamanho() {
-    if (tamanhoSenha < 20) {
-            // tamanhoSenha = tamanhoSenha + 1;
-            tamanhoSenha++;
-    }
-    numeroSenha.textContent = tamanhoSenha;
-    geraSenha();
-}21
-
+// Campo da senha
 const campoSenha = document.querySelector('#campo-senha');
 
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+// Letras disponíveis
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+// Gera a primeira senha
 geraSenha();
 
-function geraSenha(){
-    let senha = '';
- for (let i = 0; i < tamanhoSenha;i++) {
-     let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-     numeroAleatorio = Math.floor(numeroAleatorio);
-     senha = senha + letrasMaiusculas[numeroAleatorio]
- }
+// Diminuir tamanho
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha--;
+    }
 
- campoSenha.value = senhas;
- 
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+// Aumentar tamanho
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        tamanhoSenha++;
+    }
+
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+// Gerar senha
+function geraSenha() {
+
+    let senha = '';
+
+    for (let i = 0; i < tamanhoSenha; i++) {
+
+        let numeroAleatorio = Math.floor(Math.random() * letrasMaiusculas.length);
+
+        senha += letrasMaiusculas[numeroAleatorio];
+    }
+
+    campoSenha.value = senha;
 }
 
